ee3d7c7e71f311e975a204a8dfc3ec6a2bc9ef37
Anthonius Munthi <me@itstoni.com>
2024-04-29 14:26:49 +0800

    fix(ci): fixing build container image action :bug:

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /srv/api
 source /usr/local/bin/api-entrypoint
+
+# TODO remove this line fixed in goss testing
+exec /srv/api/bin/console doctrine:schema:update --force --complete
